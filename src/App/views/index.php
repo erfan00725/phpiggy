edf176a5aace8f2b08fdcb02137ee87db3f9b976
erfan00725 <erfansedighi84@gmail.com>
2024-02-16 16:23:35 +0330

    fix receips delete

--- src/App/views/index.php
+++ src/App/views/index.php
@@ -61,6 +61,7 @@
                             </a>
                             <form action="/transaction/<?php echo $transaction['id']; ?>/receipt/<?php echo $receipt['id']; ?>" method="POST">
                                 <input type="hidden" name="_METHOD" value="DELETE" />
+                                <?php include $this->resolve("partials/_csrf.php"); ?>
                                 <button type="submit" class="absolute -top-1 -right-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(239 68 68)" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
